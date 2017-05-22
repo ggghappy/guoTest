47e1ed16fdc203db92db590695014fbc1b6dac9d
ggghappy <1813028597@qq.com>
2017-05-22 21:11:30 +0800

    first change

---
+++ mytest/src/main/java/com/bwie/mytest/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.bwie.mytest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 类的作用：
+ * 实现思路 ：
+ * auther:  郭鸽鸽
+ * date ： 2017/5/22.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
